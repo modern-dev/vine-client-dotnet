c6e2e3e90604c85e4ff8b14d9cd1ebd90b6eda04
Bohdan Shtepan <winsatana@ya.ru>
2015-11-02 20:00:00 +0400

    Add IVineModel interface.

---
+++ ModernDev.VineClient.Shared/Model/Interfaces/IVineModel.cs
@@ -0,0 +1,16 @@
+/**
+* This file\code is part of VineClient project.
+*
+* VineClient - is an unofficial C# library for the Vine.
+* https://github.com/modern-dev/vine-client-dotnet
+*
+* Copyright (c) 2016 Bohdan Shtepan
+* http://modern-dev.com/
+*
+* Licensed under the GPLv3 license.
+*/
+
+namespace ModernDev.VineClient
+{
+    public interface IVineModel { }
+}
