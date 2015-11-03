6dfecf9c5607f5a022c8f0bf7e6e7979fc514ff9
Bohdan Shtepan <winsatana@ya.ru>
2015-11-03 20:00:00 +0400

    Add Loops class.

---
+++ ModernDev.VineClient.Shared/Model/Loops.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * This file\code is part of VineClient project.
+ *
+ * VineClient - is an unofficial C# library for the Vine.
+ * https://github.com/modern-dev/vine-client-dotnet
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ModernDev.VineClient
+{
+    [DataContract]
+    [DebuggerDisplay("Loops")]
+    public class Loops : IVineModel
+    {
+        [DataMember]
+        [JsonProperty("count")]
+        public double Count { get; set; }
+
+        [DataMember]
+        [JsonProperty("velocity")]
+        public double Velocity { get; set; }
+
+        [DataMember]
+        [JsonProperty("onFire")]
+        public bool OnFire { get; set; }
+    }
+}
