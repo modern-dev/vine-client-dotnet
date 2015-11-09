eff249145b64eaa8753145c32211f6fd1d55e18c
Bohdan Shtepan <winsatana@ya.ru>
2015-11-09 20:00:00 +0400

    Add UserSettings class.

---
+++ ModernDev.VineClient.Portable/Model/UserSettings.cs
@@ -0,0 +1,27 @@
+/**
+ * This file\code is part of VineClient project.
+ *
+ * VineClient - is an unofficial C# library for the Vine.
+ * https://github.com/modern-dev/vine-client-dotnet
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ModernDev.VineClient
+{
+    [DataContract]
+    [DebuggerDisplay("UserSettings {Private}")]
+    public class UserSettings
+    {
+        [DataMember]
+        [JsonProperty("private")]
+        public bool Private { get; set; }
+    }
+}
