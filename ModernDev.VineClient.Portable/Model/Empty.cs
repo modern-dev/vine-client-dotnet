49624fedd37b4df5c201c2f17e6500e094cb553b
Bohdan Shtepan <winsatana@ya.ru>
2016-05-02 04:16:30 +0300

    Adds Empty class.

---
+++ ModernDev.VineClient.Portable/Model/Empty.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * This file\code is part of VineClient project.
+ *
+ * VineClient - is an unofficial C# library for the Vine.
+ * https://github.com/modern-dev/vine-client-dotnet
+ *
+ * Copyright (c) 2016 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the GPLv3 license.
+ */
+
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace ModernDev.VineClient
+{
+    [DataContract]
+    [DebuggerDisplay("Empty")]
+    public class Empty
+    {
+        
+    }
+}
