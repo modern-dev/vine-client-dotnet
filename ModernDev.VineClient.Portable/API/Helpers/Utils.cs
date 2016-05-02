82e0a2397bb73721214a609e2a454bd16e92c68d
Bohdan Shtepan <winsatana@ya.ru>
2016-05-02 04:54:10 +0300

    Removes NormalizeRequestParams and ObjToString methods; Adds Add method.

--- ModernDev.VineClient.Portable/API/Helpers/Utils.cs
+++ ModernDev.VineClient.Portable/API/Helpers/Utils.cs
@@ -10,6 +10,7 @@
  * Licensed under the GPLv3 license.
  */
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -18,20 +19,12 @@ namespace ModernDev.VineClient
 {
     internal static class Utils
     {
-        internal static Dictionary<string, string> NormalizeRequestParams(Dictionary<string, object> reqParams)
-            => reqParams.Where(kvp => kvp.Value != null).ToDictionary(kvp => kvp.Key, kvp => ObjToStr(kvp.Value));
-
         internal static string GetQueryString(Dictionary<string, string> queryParams)
             => string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={WebUtility.UrlEncode(kvp.Value)}"));
 
-        internal static string ObjToStr(object obj)
+        internal static void Add(this List<Tuple<string, object, bool>> @this, string str, object obj, bool boolean = false)
         {
-            if (obj is bool)
-            {
-                return (bool)obj ? "1" : "0";
-            }
-
-            return obj.ToString();
+            @this.Add(Tuple.Create(str, obj, boolean));
         }
     }
 }
